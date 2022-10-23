27704dce8df87bf30c21bf948b1715336cad64ca
Adesida Leye <adeleyeadesida@gmail.com>
2022-10-23 14:56:55 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,24 @@
+let color = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'B', 'C', 'D', 'E', 'F'];
+let button = document.getElementById('btn');
+let colorCode = document.getElementById('code');
+let navH3 = document.getElementById('nav-h3');
+let navFirst = document.getElementById('li');
+let navSecond = document.getElementById('li2');
+
+
+button.addEventListener('click', () =>{
+    let hexColor = '#';
+    for(let i = 0; i < 6; i++){
+        hexColor += color[random()];
+    }
+    
+    navH3.style.color = hexColor;
+    navFirst.style.color =hexColor;
+    navSecond.style.color = hexColor;
+    colorCode.textContent = hexColor;
+    colorCode.style.color = hexColor;
+    document.body.style.backgroundColor = hexColor;
+})
+let random = function (){
+    return Math.floor(Math.random() * color.length);
+}
